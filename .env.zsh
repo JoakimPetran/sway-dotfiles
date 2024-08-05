9994ddefd5519521335d166fdedfa04bc5b4ecc8
JoakimPetran <petran@tutamail.com>
2024-08-05 05:20:22 +0300

    first commit

---
+++ .env.zsh
@@ -0,0 +1,3 @@
+export VOLTA_HOME="$HOME/.volta"
+export GOPATH="$HOME/.go"
+export PATH="$HOME/.local/bin:$HOME/.local/go/bin:$VOLTA_HOME/bin:$PATH"
---
+++ Scripts/wtyper.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+while true; do
+	sleep 300
+	wtype -k Scroll_Lock -k Scroll_Lock
+done
