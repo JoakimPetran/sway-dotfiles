44e01b17e3f16c75838060a19793046b64e4a925
JoakimPetran <petran@tutamail.com>
2024-10-23 04:27:12 +0300

    configs

--- .env.zsh
+++ .env.zsh
@@ -1,3 +1,18 @@
 export VOLTA_HOME="$HOME/.volta"
 export GOPATH="$HOME/.go"
 export PATH="$HOME/.local/bin:$HOME/.local/go/bin:$VOLTA_HOME/bin:$PATH"
+
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM=wayland-egl
+export CLUTTER_BACKEND=wayland
+export ECORE_EVAS_ENGINE=wayland-egl
+export ELM_ENGINE=wayland_egl
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
+export NO_AT_BRIDGE=1
+
+export QT_QPA_PLATFORMTHEME=qt5ct
+
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
